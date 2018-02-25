dd84a9a4a361009f736fc63f1a1e64335d66fed5
ashiqul mostofa <ashiqulmostofa@ashiquls-MacBook-Pro.local>
2018-02-25 19:17:21 +0800

    copied Main.java file to this directory

--- codejam.sh
+++ codejam.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 javac -d $(pwd)   ~/IdeaProjects/problem_solving/output/Main.java
+cp ~/IdeaProjects/problem_solving/output/Main.java Main.java
 if [ ! -e "${2}" ] ; then
     touch "${2}"
 fi
